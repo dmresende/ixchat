500c0756ce618fabe85b4be0a07b1ca1642df735
Douglas Resende <douglasmresende@gmail.com>
2024-10-19 18:25:29 -0300

    :boom: fix: Reverting changes

--- src/config/connectionDB.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const connectionDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
-}
-
-export default connectionDB

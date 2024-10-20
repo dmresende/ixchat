a4b0e8ba57b2b3f48f8199202ee83b86c8615947
Douglas Resende <douglasmresende@gmail.com>
2024-10-20 01:05:33 -0300

    fix: :rotating_light: Small fixes

--- ixchat-front-end/src/app/chat/page.tsx
+++ ixchat-front-end/src/app/chat/page.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image';
 
 export default function Chat() {
   return (
-
-    <div className=" flex justify-center items-center w-screen h-screen bg-gradient-to-r from-blue-500 to-purple-600">
-      <div className='h-[70%] w-[70%] lg:w-[60%]  bg-background rounded-xl '>
-
-        <div className=' p-4 flex justify-between bg-red-500'>
-          <h1>mensagem</h1>
-          <h1>icon</h1>
-
+    <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-br from-blue-800 via-purple-700 to-pink-700">
+      < div className="text-black h-[80%] w-[80%] lg:w-[60%] bg-white rounded-xl shadow-lg" >
+        {/* Cabeçalho do chat */}
+        < div className="p-4 flex justify-between items-center border-b" >
+          <h1 className="text-lg font-semibold">Mensagens</h1>
+          <div className="flex items-center space-x-2">
+            <div className="flex flex-col text-right">
+              <span className="font-medium">Maria</span>
+              <span className="text-sm text-gray-500">maria@gmail.com</span>
         </div>
 
         <div className='bg-black grid grid-cols-[200px_1fr] h-full'>

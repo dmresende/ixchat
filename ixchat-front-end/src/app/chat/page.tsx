bc15e532f401c49e7d4eb595af4c4a524fa4f748
Douglas Resende <douglasmresende@gmail.com>
2024-10-21 12:15:50 -0300

    :lipstick: fix: changed page chat

--- ixchat-front-end/src/app/chat/page.tsx
+++ ixchat-front-end/src/app/chat/page.tsx
@@ -1,25 +1,32 @@
 "use client";
-import { useState, useRef, use, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useAuth } from "@/hooks/useAlth";
 import toast from "react-hot-toast";
 import { api } from '@/utils/api'
 import { useRouter } from 'next/navigation';
-interface UserTypes {
-  id: number
-  name: string
-  username: string
-  photo: string
+
+interface User {
+  id: number;
+  name: string;
+  photo: string;
+  isOnline: boolean;
+}
+
+interface Message {
+  id: number;
+  content: string;
+  timestamp: string;
+  isSent: boolean;
 }
 
 const Chat = () => {
   const [menuVisible, setMenuVisible] = useState(false);
   const { logout, data } = useAuth();
-  const [users, setUsers] = useState<UserTypes[]>([]);
-  const [visibleUsersCount, setVisibleUsersCount] = useState(9); // Número inicial de usuários visíveis
+  const [users, setUsers] = useState<User[]>([]);
   const router = useRouter();
-
-  console.log('users: ', users)
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [newMessage, setNewMessage] = useState('');
 
   const handleLogout = () => {
     logout();
@@ -29,29 +36,33 @@ const Chat = () => {
 
   const getDataUsers = async () => {
     try {
-      const rest = await api.get("/users/");
-      setUsers(rest.data);
-
+      const response = await api.get("/users/");
+      setUsers(response.data);
     } catch (error) {
-      console.error(error);
+      console.error("Erro ao buscar usuários:", error);
     }
   }
 
-  const loadMoreUsers = () => {
-    setVisibleUsersCount(prevCount => prevCount + 2);
-  };
-
   useEffect(() => {
-    getDataUsers()
-  }, [])
+    getDataUsers();
+  }, []);
 
-  const toggleMenu = () => {
-    setMenuVisible((prev) => !prev);
+  const handleSendMessage = () => {
+    if (newMessage.trim() !== '') {
+      const newMessageObj: Message = {
+        id: messages.length + 1,
+        content: newMessage,
+        timestamp: new Date().toLocaleTimeString(),
+        isSent: true
+      };
+      setMessages([...messages, newMessageObj]);
+      setNewMessage('');
+    }
   };
 
   return (
-    <div className=" flex justify-center items-center w-screen h-screen bg-gradient-to-r from-blue-500 to-purple-600">
-      <div className='h-[70%] w-[70%] lg:w-[60%]  bg-background rounded-xl '>
+    <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <div className='h-[70%] w-[70%] lg:w-[60%] bg-background rounded-xl'>
         {/* Cabeçalho */}
         <div className="p-4 flex justify-between items-center bg-white border-b">
           <h1 className="text-xl font-bold text-gray-800">Mensagens</h1>
@@ -63,18 +74,18 @@ const Chat = () => {
             <div className="relative">
               <img
                 src={data?.user?.photo}
-                onClick={toggleMenu}
+                onClick={() => setMenuVisible(!menuVisible)}
                 alt="Foto de perfil"
                 className="w-10 h-10 rounded-full cursor-pointer"
               />
               {menuVisible && (
-                <div className="absolute left-full mt-2 w-48 bg-white rounded-lg shadow-lg z-10 transition-opacity duration-300 opacity-100">
-                  <Link href="/" className="block px-4 py-2 text-black hover:bg-gray-100 transition">
-                    x
+                <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-10">
+                  <Link href="/" className="block px-4 py-2 text-black hover:bg-gray-100">
+                    Chat
                   </Link>
-                  <a onClick={handleLogout} className="block px-4 py-2 text-red-500 hover:bg-gray-100 transition">
-                    sair
-                  </a>
+                  <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100">
+                    Sair
+                  </button>
                 </div>
               )}
             </div>
@@ -82,66 +93,49 @@ const Chat = () => {
         </div>
 
         {/* Corpo principal */}
-        <div className="grid grid-cols-[250px_1fr] h-full">
-
-
-          {/* Barra lateral de conversas */}
-          <div className="bg-gray-100 p-4 border-r">
-            {users?.length > 0 && users.map((user) => (
-              <div key={user.id} className="mb-4">
-                <div className="flex items-center p-2 bg-blue-50 rounded-lg cursor-pointer hover:bg-blue-100">
-                  <div className="w-12 h-12 rounded-full bg-yellow-500 mr-3"></div>
+        <div className="grid grid-cols-[250px_1fr] h-[calc(100%-64px)]">
+          {/* Lista de usuários */}
+          <div className="bg-gray-100 p-4 border-r overflow-y-auto">
+            {users.map((user) => (
+              <div key={user.id} className="mb-4 p-2 bg-blue-50 rounded-lg hover:bg-blue-100">
+                <div className="flex items-center">
+                  <img src={user.photo} alt={user.name} className="w-10 h-10 rounded-full mr-3" />
                   <div>
                     <h2 className="font-semibold text-gray-800">{user.name}</h2>
+                    <span className="text-sm text-gray-500">{user.isOnline ? "Online" : "Offline"}</span>
                   </div>
                 </div>
               </div>
             ))}
-            {/* {users.length > visibleUsersCount && ( // Verifica se há mais usuários para mostrar
-              // <button
-              //   onClick={loadMoreUsers}
-              //   className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-              // >
-              //   Carregar mais
-              // </button>
-            )} */}
           </div>
 
-          {/* Área do chat */}
+          {/* Área de chat */}
           <div className="flex flex-col bg-gray-50">
-            {/* Cabeçalho do chat */}
-            <div className="p-4 flex items-center bg-white border-b">
-              <div className="w-12 h-12 rounded-full bg-yellow-500 mr-3"></div>
-              <div>
-                <h2 className="font-semibold text-gray-800">Cláudia</h2>
-                <span className="text-sm text-green-500">Online</span>
-              </div>
-            </div>
-
             {/* Mensagens */}
-            <div className="flex-1 p-4 space-y-4 overflow-y-auto bg-white">
-              <div className="flex justify-start">
-                <div className="bg-gray-200 p-3 rounded-lg max-w-[60%]">
-                  <p className="text-gray-800">Oi, tudo bem?</p>
-                  <span className="text-xs text-gray-500">Hoje, 08:30</span>
+            <div className="flex-1 p-4 space-y-4 overflow-y-auto">
+              {messages.map((message, index) => (
+                <div key={index} className={`flex ${message.isSent ? 'justify-end' : 'justify-start'}`}>
+                  <div className={`p-3 rounded-lg max-w-[60%] ${message.isSent ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}>
+                    <p>{message.content}</p>
+                    <span className="text-xs">{message.timestamp}</span>
+                  </div>
                 </div>
-              </div>
-              <div className="flex justify-end">
-                <div className="bg-blue-500 text-white p-3 rounded-lg max-w-[60%]">
-                  <p>Sim e você?</p>
-                  <span className="text-xs text-gray-200">Hoje, 08:31</span>
-                </div>
-              </div>
+              ))}
             </div>
 
-            {/* Caixa de texto */}
-            <div className="p-4 bg-gray-100 flex items-center">
+            {/* Input de mensagem */}
+            <div className="text-black p-4 bg-gray-100 flex items-center">
               <input
                 type="text"
-                placeholder="Digite sua mensagem aqui"
-                className="flex-1 p-3 border rounded-lg outline-none focus:ring-2 focus:ring-blue-500 transition"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                placeholder="Digite sua mensagem"
+                className="flex-1 p-3 border rounded-lg"
               />
-              <button className="ml-3 bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition-all">
+              <button 
+                onClick={handleSendMessage}
+                className="ml-3 bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600"
+              >
                 Enviar
               </button>
             </div>
@@ -149,7 +143,6 @@ const Chat = () => {
         </div>
       </div>
     </div>
-
   );
 };
 
--- ixchat-front-end/src/app/login/page.tsx
+++ ixchat-front-end/src/app/login/page.tsx
@@ -9,12 +9,17 @@ const Login = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [stayConnected, setStayConnected] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const { login } = useAuth()
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await login(username, password, router);
     // lógica para autenticação
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -68,7 +73,7 @@ const Login = () => {
             </label>
             <div className="relative mt-1">
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 id="password"
                 className="text-slate-700 block w-full border border-gray-300 rounded-md px-4 py-2 focus:ring-indigo-500 focus:border-indigo-500"
                 placeholder="**********"
@@ -76,27 +81,44 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
-              <span className="absolute inset-y-0 right-0 flex items-center pr-3">
-                <svg
-                  className="h-5 w-5 text-gray-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 12v2m0-2v2m0 0h4m-4 0h-4"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l4 4m0 0l-4-4m4 4V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2v-6"
-                  />
-                </svg>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={togglePasswordVisibility}>
+                {showPassword ? (
+                  <svg
+                    className="h-5 w-5 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="h-5 w-5 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                    />
+                  </svg>
+                )}
               </span>
             </div>
           </div>
